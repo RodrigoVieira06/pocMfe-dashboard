7e7adce324c8633d85ff085e7f45075dc1fc1920
RodrigoVieira06 <rodrigovlima06@gmail.com>
2025-02-09 20:37:26 -0300

    feat: addign the first application version

---
+++ src/DashboardPage.tsx
@@ -0,0 +1,13 @@
+// dashboard-app/src/DashboardPage.tsx
+import React from "react";
+
+const DashboardPage: React.FC = () => {
+  return (
+    <div style={{ padding: "20px" }}>
+      <h2>Dashboard</h2>
+      <p>Conteúdo da dashboard: informações, gráficos, etc.</p>
+    </div>
+  );
+};
+
+export default DashboardPage;
---
+++ vite.config.ts
@@ -0,0 +1,24 @@
+// dashboard-app/vite.config.ts
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import federation from "@originjs/vite-plugin-federation";
+
+export default defineConfig({
+  plugins: [
+    react(),
+    federation({
+      name: "dashboardApp",
+      filename: "remoteEntry.js",
+      exposes: {
+        "./DashboardPage": "./src/DashboardPage"
+      },
+      shared: ["react", "react-dom"]
+    })
+  ],
+  build: {
+    modulePreload: false,
+    target: "esnext",
+    minify: false,
+    cssCodeSplit: false
+  }
+});
